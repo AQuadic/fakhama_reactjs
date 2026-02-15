452fabf22606e460c7379a510f2efe011e70029e
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2026-02-15 13:35:06 +0200

    adds trip details responsive

---
+++ src/components/icons/BackArrow.tsx
@@ -0,0 +1,9 @@
+const BackArrow = () => {
+    return (
+        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20.91 31.9201L27.43 25.4001C28.2 24.6301 28.2 23.3701 27.43 22.6001L20.91 16.0801" stroke="#121212" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    )
+}
+
+export default BackArrow
